3d8ff06e50ed2a81ed9dc92514ac2dff7f38ab70
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-02-14 12:09:47 +0100

    ci: v0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,8 +85,8 @@ android {
         applicationId = "si.maev.twa"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         // The name for the application
         resValue("string", "appName", TwaManifest.name)
