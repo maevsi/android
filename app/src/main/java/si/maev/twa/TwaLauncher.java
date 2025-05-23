a14780012f5aeb37bd913ed00fd45ae4e2cf2a42
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-05-23 11:53:38 +0200

    refactor(twa): update activity and launcher

--- app/src/main/java/si/maev/twa/TwaLauncher.java
+++ app/src/main/java/si/maev/twa/TwaLauncher.java
@@ -45,6 +45,11 @@ import com.google.androidbrowserhelper.trusted.SharedPreferencesTokenStore;
 import com.google.androidbrowserhelper.trusted.TwaProviderPicker;
 import com.google.androidbrowserhelper.trusted.WebViewFallbackActivity;
 import com.google.androidbrowserhelper.trusted.splashscreens.SplashScreenStrategy;
+import com.google.androidbrowserhelper.BuildConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Encapsulates the steps necessary to launch a Trusted Web Activity, such as establishing a
@@ -53,7 +58,13 @@ import com.google.androidbrowserhelper.trusted.splashscreens.SplashScreenStrateg
 public class TwaLauncher {
     private static final String TAG = "TwaLauncher";
 
-    private static final int DEFAULT_SESSION_ID = 96375;
+    private static final Map<Integer, Integer> mTaskIdToSessionId = new HashMap<>();
+
+    private static final String EXTRA_STARTUP_UPTIME_MILLIS =
+            "org.chromium.chrome.browser.customtabs.trusted.STARTUP_UPTIME_MILLIS";
+
+    private static final String EXTRA_ANDROID_BROWSER_HELPER_VERSION =
+            "org.chromium.chrome.browser.ANDROID_BROWSER_HELPER_VERSION";
 
     public static final FallbackStrategy CCT_FALLBACK_STRATEGY =
             (context, twaBuilder, providerPackage, completionCallback) -> {
@@ -99,9 +110,11 @@ public class TwaLauncher {
     @Nullable
     private CustomTabsSession mSession;
 
-    private TokenStore mTokenStore;
+    private final TokenStore mTokenStore;
 
     private boolean mDestroyed;
+
+    private long mStartupUptimeMillis;
 
     public interface FallbackStrategy {
         void launch(Context context,
@@ -119,11 +132,19 @@ public class TwaLauncher {
     }
 
     /**
+     * Same as above, but also allows to specify a task id to distinguish several sessions running
+     * for the same TWA app.
+     */
+    public TwaLauncher(Context context, @Nullable Integer taskId) {
+        this(context, null, taskId);
+    }
+
+    /**
      * Same as above, but also allows to specify a browser to launch. If specified, it is assumed to
      * support TWAs.
      */
-    public TwaLauncher(Context context, @Nullable String providerPackage) {
-        this(context, providerPackage, DEFAULT_SESSION_ID,
+    public TwaLauncher(Context context, @Nullable String providerPackage, @Nullable Integer taskId) {
+        this(context, providerPackage, taskId,
                 new SharedPreferencesTokenStore(context));
     }
 
@@ -131,10 +152,10 @@ public class TwaLauncher {
      * Same as above, but also accepts a session id. This allows to launch multiple TWAs in the same
      * task.
      */
-    public TwaLauncher(Context context, @Nullable String providerPackage, int sessionId,
+    public TwaLauncher(Context context, @Nullable String providerPackage, @Nullable Integer taskId,
                        TokenStore tokenStore) {
         mContext = context;
-        mSessionId = sessionId;
+        mSessionId = makeSessionId(taskId);
         mTokenStore = tokenStore;
         if (providerPackage == null) {
             TwaProviderPicker.Action action =
@@ -145,6 +166,19 @@ public class TwaLauncher {
             mProviderPackage = providerPackage;
             mLaunchMode = TwaProviderPicker.LaunchMode.TRUSTED_WEB_ACTIVITY;
         }
+    }
+
+    private static Integer makeSessionId(@Nullable Integer taskId) {
+        if(taskId == null) return Integer.MAX_VALUE;
+
+        Integer sessionId = mTaskIdToSessionId.get(taskId);
+        if(sessionId == null) {
+            Random random = new Random();
+            sessionId = random.nextInt(Integer.MAX_VALUE);
+            mTaskIdToSessionId.put(taskId, sessionId);
+        }
+
+        return sessionId;
     }
 
     /**
@@ -163,15 +197,15 @@ public class TwaLauncher {
     /**
      * Similar to {@link #launch(Uri)}, but allows more customization.
      *
-     * @param twaBuilder           {@link TrustedWebActivityIntentBuilder} containing the url to open, along with
-     *                             optional parameters: status bar color, additional trusted origins, etc.
-     * @param customTabsCallback   {@link CustomTabsCallback} to get messages from the browser, use
-     *                             for quality enforcement.
+     * @param twaBuilder {@link TrustedWebActivityIntentBuilder} containing the url to open, along with
+     * optional parameters: status bar color, additional trusted origins, etc.
+     * @param customTabsCallback {@link CustomTabsCallback} to get messages from the browser, use
+     * for quality enforcement.
      * @param splashScreenStrategy {@link SplashScreenStrategy} to use for showing splash screens,
-     *                             null if splash screen not needed.
-     * @param completionCallback   Callback triggered when the url has been opened.
-     * @param fallbackStrategy     Called when there is no TWA provider available or when launching
-     *                             the Trusted Web Activity fails.
+     * null if splash screen not needed.
+     * @param completionCallback Callback triggered when the url has been opened.
+     * @param fallbackStrategy Called when there is no TWA provider available or when launching
+     * the Trusted Web Activity fails.
      */
     public void launch(TrustedWebActivityIntentBuilder twaBuilder,
                        CustomTabsCallback customTabsCallback,
@@ -191,7 +225,7 @@ public class TwaLauncher {
 
         // Remember who we connect to as the package that is allowed to delegate notifications
         // to us.
-        if (!ChromeOsSupport.isRunningOnArc(mContext.getPackageManager())) {
+        if (!ChromeOsSupport.isRunningOnArc(mContext.getPackageManager()) && mProviderPackage != null) {
             // Since ChromeOS may not follow this path when launching a TWA, we set the verified
             // provider in DelegationService instead.
             mTokenStore.store(Token.create(mProviderPackage, mContext.getPackageManager()));
@@ -202,13 +236,13 @@ public class TwaLauncher {
      * Similar to {@link #launch(Uri)}, but allows more customization. Uses a Custom Tabs fallback
      * when a TWA provider is not available or when launching a TWA fails.
      *
-     * @param twaBuilder           {@link TrustedWebActivityIntentBuilder} containing the url to open, along with
-     *                             optional parameters: status bar color, additional trusted origins, etc.
-     * @param customTabsCallback   {@link CustomTabsCallback} to get messages from the browser, use
-     *                             for quality enforcement.
+     * @param twaBuilder {@link TrustedWebActivityIntentBuilder} containing the url to open, along with
+     * optional parameters: status bar color, additional trusted origins, etc.
+     * @param customTabsCallback {@link CustomTabsCallback} to get messages from the browser, use
+     * for quality enforcement.
      * @param splashScreenStrategy {@link SplashScreenStrategy} to use for showing splash screens,
-     *                             null if splash screen not needed.
-     * @param completionCallback   Callback triggered when the url has been opened.
+     * null if splash screen not needed.
+     * @param completionCallback Callback triggered when the url has been opened.
      */
     public void launch(TrustedWebActivityIntentBuilder twaBuilder,
                        CustomTabsCallback customTabsCallback,
@@ -227,8 +261,15 @@ public class TwaLauncher {
             splashScreenStrategy.onTwaLaunchInitiated(mProviderPackage, twaBuilder);
         }
 
+        Runnable onSessionCreatedRunnable = () -> {
+            assert mSession != null;
+            mSession.validateRelationship(CustomTabsService.RELATION_USE_AS_ORIGIN,
+                    twaBuilder.getUri(), null);
+        };
+//                launchWhenSessionEstablished(twaBuilder, splashScreenStrategy, completionCallback);
+
         if (mSession != null) {
-            launchWhenRelationshipValidated(twaBuilder, splashScreenStrategy, completionCallback);
+            onSessionCreatedRunnable.run();
             return;
         }
 
@@ -240,19 +281,18 @@ public class TwaLauncher {
         };
 
         if (mServiceConnection == null) {
-            mServiceConnection = new TwaCustomTabsServiceConnection(customTabsCallback, twaBuilder.getUri());
+            mServiceConnection = new TwaCustomTabsServiceConnection(customTabsCallback);
         }
 
         mServiceConnection.setSessionCreationRunnables(
-                onSessionCreationFailedRunnable
-        );
+                onSessionCreatedRunnable, onSessionCreationFailedRunnable);
         CustomTabsClient.bindCustomTabsServicePreservePriority(
                 mContext, mProviderPackage, mServiceConnection);
     }
 
-    public void launchWhenRelationshipValidated(TrustedWebActivityIntentBuilder twaBuilder,
-                                                @Nullable SplashScreenStrategy splashScreenStrategy,
-                                                @Nullable Runnable completionCallback) {
+    void launchWhenSessionEstablished(TrustedWebActivityIntentBuilder twaBuilder,
+                                      @Nullable SplashScreenStrategy splashScreenStrategy,
+                                      @Nullable Runnable completionCallback) {
         if (mSession == null) {
             throw new IllegalStateException("mSession is null in launchWhenSessionEstablished");
         }
@@ -279,6 +319,11 @@ public class TwaLauncher {
         headers.putString("vibetype-platform", "android");
         intent.getIntent().putExtra(Browser.EXTRA_HEADERS, headers);
 
+        if (mStartupUptimeMillis != 0) {
+            intent.getIntent().putExtra(EXTRA_STARTUP_UPTIME_MILLIS, mStartupUptimeMillis);
+        }
+        intent.getIntent().putExtra(
+                EXTRA_ANDROID_BROWSER_HELPER_VERSION, BuildConfig.LIBRARY_VERSION);
         FocusActivity.addToIntent(intent.getIntent(), mContext);
         intent.launchTrustedWebActivity(mContext);
 
@@ -309,17 +354,27 @@ public class TwaLauncher {
         return mProviderPackage;
     }
 
+    /**
+     * Sets the timestamp (in SystemClock.uptimeMillis()) when the TWA launcher
+     * activity was created. This timestamp is used to report the full startup
+     * duration to the browser.
+     */
+    public void setStartupUptimeMillis(long startupUptimeMillis) {
+        mStartupUptimeMillis = startupUptimeMillis;
+    }
+
     private class TwaCustomTabsServiceConnection extends CustomTabsServiceConnection {
+        private Runnable mOnSessionCreatedRunnable;
         private Runnable mOnSessionCreationFailedRunnable;
         private final CustomTabsCallback mCustomTabsCallback;
-        private final Uri mLaunchUri;
 
-        TwaCustomTabsServiceConnection(CustomTabsCallback callback, Uri launchUri) {
+        TwaCustomTabsServiceConnection(CustomTabsCallback callback) {
             mCustomTabsCallback = callback;
-            mLaunchUri = launchUri;
         }
 
-        private void setSessionCreationRunnables(@Nullable Runnable onFailure) {
+        private void setSessionCreationRunnables(@Nullable Runnable onSuccess,
+                                                 @Nullable Runnable onFailure) {
+            mOnSessionCreatedRunnable = onSuccess;
             mOnSessionCreationFailedRunnable = onFailure;
         }
 
@@ -334,10 +389,9 @@ public class TwaLauncher {
             try {
                 mSession = client.newSession(mCustomTabsCallback, mSessionId);
 
-                if (mSession != null) {
-                    mSession.validateRelationship(CustomTabsService.RELATION_USE_AS_ORIGIN,
-                            mLaunchUri, null);
-                } else if (mOnSessionCreationFailedRunnable != null) {
+                if (mSession != null && mOnSessionCreatedRunnable != null) {
+                    mOnSessionCreatedRunnable.run();
+                } else if (mSession == null && mOnSessionCreationFailedRunnable != null) {
                     mOnSessionCreationFailedRunnable.run();
                 }
             } catch (RuntimeException e) {
@@ -345,6 +399,7 @@ public class TwaLauncher {
                 mOnSessionCreationFailedRunnable.run();
             }
 
+            mOnSessionCreatedRunnable = null;
             mOnSessionCreationFailedRunnable = null;
         }
 
