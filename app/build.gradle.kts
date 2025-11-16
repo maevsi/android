49e12acf0283ffe4d6221a20d588a5019c1b3963
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-11-16 01:54:54 +0100

    ci: update to jdk v21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -221,13 +221,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_1_8
+            jvmTarget = JvmTarget.JVM_21
         }
     }
 
