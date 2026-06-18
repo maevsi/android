b09ca1c74ae972dec27a2b6c6736cf6205cb7712
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-06-18 14:05:21 +0200

    refactor(deps): update to android-browser-helper v2.7.2

--- app/src/main/java/si/maev/twa/LauncherActivity.kt
+++ app/src/main/java/si/maev/twa/LauncherActivity.kt
@@ -2,9 +2,8 @@ package si.maev.twa
 
 import com.google.androidbrowserhelper.trusted.SessionStore
 import com.google.androidbrowserhelper.trusted.SharedPreferencesTokenStore
-import si.maev.twa.androidbrowserhelper.LauncherActivity
 
-class LauncherActivity : LauncherActivity() {
+class LauncherActivity : com.google.androidbrowserhelper.trusted.LauncherActivity() {
 
     override fun createTwaLauncher(): TwaLauncher {
         return TwaLauncher(
--- app/src/main/java/si/maev/twa/TwaLauncher.kt
+++ app/src/main/java/si/maev/twa/TwaLauncher.kt
@@ -5,15 +5,16 @@ import android.os.Bundle
 import android.provider.Browser
 import androidx.browser.trusted.TokenStore
 import androidx.browser.trusted.TrustedWebActivityIntent
-import si.maev.twa.androidbrowserhelper.TwaLauncher
 
 class TwaLauncher(
     context: Context,
     providerPackage: String?,
     sessionId: Int?,
     tokenStore: TokenStore
-) : TwaLauncher(context, providerPackage, sessionId, tokenStore) {
-    override fun getIntent(intent: TrustedWebActivityIntent): TrustedWebActivityIntent {
+) : com.google.androidbrowserhelper.trusted.TwaLauncher(
+    context, providerPackage, sessionId, tokenStore
+) {
+    override fun onPrepareIntent(intent: TrustedWebActivityIntent): TrustedWebActivityIntent {
         val headers = Bundle()
         headers.putString("vibetype_platform", "android")
         intent.intent.putExtra(Browser.EXTRA_HEADERS, headers)
